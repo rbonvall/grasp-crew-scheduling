102ba5f474767eb1a2b911b88908ce738348d03d
Roberto Bonvallet <rbonvall+github@gmail.com>
2008-10-06 19:33:26 -0400

    Rotation generation method

--- csp.py
+++ csp.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from collections import defaultdict
+range = xrange
 
 class CrewSchedulingProblem:
     """Crew Scheduling problem instance from ORLIB."""
@@ -16,6 +17,24 @@ class CrewSchedulingProblem:
             # Reindex to start from 0, not from 1 as in ORLIB instances
             self.transition_costs[i - 1, j - 1] = cost
             self.possible_transitions[i - 1].append(j - 1)
+
+    def generate_rotations(self, from_rotation=()):
+        if from_rotation:
+            candidates = self.possible_transitions[from_rotation[-1]]
+        else:
+            candidates = range(len(self.tasks))
+
+        for task in candidates:
+            rotation = from_rotation + (task,)
+            start_time, _  = self.tasks[rotation[0]]
+            _, finish_time = self.tasks[rotation[-1]]
+            if finish_time - start_time > self.time_limit:
+                continue
+            
+            yield rotation
+            for r in self.generate_rotations(rotation):
+                yield r
+
 
 
 def main():
@@ -41,6 +60,29 @@ def main():
     print 'Time limit: %d  ' % csp.time_limit
     print 'Transitions: %d ' % len(csp.transition_costs)
 
+    # test generation of rotations
+    data = []
+    for n, rotation in enumerate(csp.generate_rotations()):
+        cost = sum(csp.transition_costs[t] for t in zip(rotation, rotation[1:]))
+        start_time, _  = csp.tasks[rotation[0]]
+        _, finish_time = csp.tasks[rotation[-1]]
+        duration = finish_time - start_time
+        r = str(tuple(x+1 for x in rotation))
+        data.append((r, cost, duration))
+
+    try:
+        from ptable import Table
+    except ImportError:
+        pass
+    else:
+        t = Table(data)
+        t.headers = ('Rotation', 'Cost', 'Duration')
+        t.align = 'lrr'
+        t.col_separator = ' | '
+        t.repeat_headers_after = 25
+        t.header_separator = True
+        t.print_table()
+    print '# of rotations: %d' % (n + 1)
 
 if __name__ == '__main__':
     main()
